15ab4d237124a277bd45cec1013f76f218b1a16e
张岩 <410306329@qq.com>
2016-11-19 20:50:54 +0800

    Add files via upload

---
+++ XuHao20161105.ino
@@ -0,0 +1,18 @@
+/*
+20161105 16:39
+McKnight
+循环一次，数字自加1
+*/
+long  A;//-2147483648~2147483647
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  A = A + 1;
+  delay(1);
+  Serial.print("A is ");
+  Serial.print(A);
+  Serial.println(".");
+}
